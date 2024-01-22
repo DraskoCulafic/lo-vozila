b19bd27bfc0de09f8c808e7f25b114fdcd47329d
DraskoCulafic <33601125+DraskoCulafic@users.noreply.github.com>
2024-01-22 14:51:56 +0000

    asd

--- app.py
+++ app.py
@@ -7,5 +7,27 @@ def hello_world():
   return "Hello World"
 
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+  return render_template('home.html')
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True, port=8080)
+=======
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return "Hello World"
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug =True, port=8080)
